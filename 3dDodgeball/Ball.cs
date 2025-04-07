997b07901a927db4206b420a0ae0230f8287b0d5
SliceJosiah <slicejosiah.vcuum.pm3@disroot.org>
2025-04-07 21:40:11 +1200

    Rename Physics1.cs to Ball.cs and remove arrays.

--- 3dDodgeball/Ball.cs
+++ 3dDodgeball/Ball.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace _3dDodgeball
 {
-    internal class Physics1
+    internal class Ball
     {
         //ball location arrays. 0 is always the player ball while balls afterwards
-        double[] ballX;
-        double[] ballY;
-        double[] ballZ;
-        double[] ballXvel;
-        double[] ballYvel;
-        double[] ballZvel;
+        double ballX;
+        double ballY;
+        double ballZ;
+        double ballXvel;
+        double ballYvel;
+        double ballZvel;
         
     }
 }
