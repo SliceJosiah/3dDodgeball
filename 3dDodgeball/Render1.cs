ad28d7d9f64fbe11f9dd5c5a9ba445f2c351f882
Yosaia <86264591+SliceJosiah@users.noreply.github.com>
2025-10-28 09:57:18 +1300

    Substituted square root of 0.5, and made changes to y axis enemy position generation.

--- 3dDodgeball/Render1.cs
+++ 3dDodgeball/Render1.cs
@@ -12,7 +12,7 @@ namespace _3dDodgeball
         private Player1 player1;
         private Enemies1 enemies1;
         private Game2 game2;
-        static readonly double vMult = Math.Sqrt(0.5);
+        static readonly double ym = Math.Sqrt(0.5);
         public Render1(Player1 player1d, Enemies1 enemies1b, Game2 game2b)
         {
             player1 = player1d;
@@ -24,12 +24,12 @@ namespace _3dDodgeball
             Graphics g = e.Graphics;
 
             g.FillRectangle(Brushes.Red, 0, 0, 10, 10);
-            g.FillRectangle(Brushes.Cyan, Convert.ToSingle(player1.playerPos * 100), game2.ClientSize.Height - Convert.ToSingle(player1.playerHeight * 100 * Math.Sqrt(0.5)), Convert.ToSingle(player1.playerWidth * 100), Convert.ToSingle(player1.playerHeight * 100 * Math.Sqrt(0.5)));
+            g.FillRectangle(Brushes.Cyan, Convert.ToSingle(player1.playerPos * 100), game2.ClientSize.Height - Convert.ToSingle(player1.playerHeight * 100 * ym), Convert.ToSingle(player1.playerWidth * 100), Convert.ToSingle(player1.playerHeight * 100 * ym));
 
             for (int i = 0; i < GameUpd1.enemyCount; i++)
             {
-                g.FillRectangle(Brushes.Red, Convert.ToSingle(enemies1.enemyPos[i] * 100), 300 - Convert.ToSingle(enemies1.enemyHeight[i] * 100 * Math.Sqrt(0.5)), Convert.ToSingle(enemies1.enemyWidth[i] * 100), Convert.ToSingle(enemies1.enemyHeight[i] * 100 * Math.Sqrt(0.5)));
-                g.FillEllipse(Brushes.Green, Convert.ToSingle((enemies1.ball[i].ballX - enemies1.ball[i].ballRadius) * 100), game2.ClientSize.Height - Convert.ToSingle((enemies1.ball[i].ballY + enemies1.ball[i].ballZ) * 100 * Math.Sqrt(0.5)), Convert.ToSingle(enemies1.ball[i].ballRadius * 200), Convert.ToSingle(enemies1.ball[i].ballRadius * 200));
+                g.FillRectangle(Brushes.Red, Convert.ToSingle(enemies1.enemyPos[i] * 100), Convert.ToSingle((3 - enemies1.enemyHeight[i] * 100) * ym), Convert.ToSingle(enemies1.enemyWidth[i] * 100), Convert.ToSingle(enemies1.enemyHeight[i] * 100 * ym));
+                g.FillEllipse(Brushes.Green, Convert.ToSingle((enemies1.ball[i].ballX - enemies1.ball[i].ballRadius) * 100), game2.ClientSize.Height - Convert.ToSingle((enemies1.ball[i].ballY + enemies1.ball[i].ballZ) * 100 * ym), Convert.ToSingle(enemies1.ball[i].ballRadius * 200), Convert.ToSingle(enemies1.ball[i].ballRadius * 200));
             }
         }
     }
