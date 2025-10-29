c4d4c6f735ca66a770ea4069112ca35d7987064e
Yosaia <86264591+SliceJosiah@users.noreply.github.com>
2025-10-29 20:40:06 +1300

    Change X/Y damping to 0.8

--- 3dDodgeball/Ball.cs
+++ 3dDodgeball/Ball.cs
@@ -78,8 +78,8 @@ namespace _3dDodgeball
                     if (bounces >= 4) { //if this is the fourth time the ball has hit the ground
                         thrown = false;
                     } else {
-                        ballXvel *= 0.7;    //damp the X velocity by x0.7
-                        ballYvel *= 0.7;    //damp the Y velocity by x0.7
+                        ballXvel *= 0.9;    //damp the X velocity by x0.9
+                        ballYvel *= 0.9;    //damp the Y velocity by x0.9
                         ballZvel *= -0.5;   //reverse the Z velocity and damp it by x0.5
                     }
                 }
