f07739bee916a69f491378ca30c80f68784ca2c3
Yosaia <86264591+SliceJosiah@users.noreply.github.com>
2025-10-29 20:40:58 +1300

    Remove useless red square

--- 3dDodgeball/Render1.cs
+++ 3dDodgeball/Render1.cs
@@ -23,7 +23,6 @@ namespace _3dDodgeball
         {
             Graphics g = e.Graphics;
 
-            g.FillRectangle(Brushes.Red, 0, 0, 10, 10);
             g.FillRectangle(Brushes.Cyan, Convert.ToSingle(player1.playerPos * 100), game2.ClientSize.Height - Convert.ToSingle(player1.playerHeight * 100 * ym), Convert.ToSingle(player1.playerWidth * 100), Convert.ToSingle(player1.playerHeight * 100 * ym));
             g.FillRectangle(Brushes.Green, 50, 0, 0, Convert.ToSingle(game2.ClientSize.Width * player1.playerHealth));
 
